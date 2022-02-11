4ab44c5ad5d368986de7c34d983a4807cf89a64a
Shahad Mahmud <shahad9381@gmail.com>
2022-02-11 21:28:19 +0600

    Time: 4 ms (70.20%), Space: 6.7 MB (64.97%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -3,7 +3,8 @@ public:
     int maxPower(string s) {
         int max=0, c=0;
         char prev = '#';
-        for(int i=0; i<s.length(); i++){
+        int len = s.length();
+        for(int i=0; i<len; i++){
             if(s[i] == prev)
                 c++;
             else{
