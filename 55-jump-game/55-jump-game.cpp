daf43433ed7511674c2df7b4aab0a21a77072ac4
Shahad Mahmud <shahad9381@gmail.com>
2022-02-16 12:07:06 +0600

    Time: 536 ms (14.07%), Space: 48.7 MB (16.00%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -18,24 +18,4 @@ public:
         
         return is_possible[0];
     }
-    
-    bool isPossible(vector<int>& nums, int pos, int size, int *mem){
-        if(pos == size - 1) return true;
-        if(pos >= size) return false;
-        if(pos != size -1 && nums[pos] == 0) return false;
-        
-        if(mem[pos] != -1) return mem[pos];
-        
-        vector<bool> temp_res;
-        for(int i=1; i<=nums[pos]; i++){
-            mem[pos] = isPossible(nums, pos + i, size, mem);
-            temp_res.push_back(mem[pos]);
-        }
-        
-        for(auto i=temp_res.begin(); i != temp_res.end(); i++)
-            if(*i == true)
-                return true;
-        
-        return false;
-    }
 };
