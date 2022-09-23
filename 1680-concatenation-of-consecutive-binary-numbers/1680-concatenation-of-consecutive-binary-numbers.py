49a7d0630742a9ab273ed97ce3bd9207aeb16cda
Shahad Mahmud <shahad9381@gmail.com>
2022-09-23 23:00:24 +0600

    Time: 8375 ms (5.51%), Space: 13.8 MB (80.31%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -4,9 +4,6 @@ class Solution:
         M=1000000007
         
         for i in range(1, n+1):
-            # res = res * 2**(floor(log(i,2)) + 1) + i
             res = ((res%M * (2**(floor(log(i,2)) + 1))%M)%M + i)%M
-            # print(f"{i} -- {ceil(log(i,2))} -- {2**ceil(log(i,2))}")
-            # print(i, res, log(i,2))
         
         return int(res)
